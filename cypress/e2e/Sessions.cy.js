7c84f9f17af2f850a394d0084d8833848cf2096c
Nibesh Upadhyay <Nibesh.Upadhyay@swifttech.com>
2025-05-09 14:55:41 +0545

    Updated Sessions

--- cypress/e2e/Sessions.cy.js
+++ cypress/e2e/Sessions.cy.js
@@ -4,20 +4,24 @@ describe('Session usage example', () => {
             cy.visit("https://bstackdemo.com/signin");
 
             cy.get('#username').type(username);
-            cy.get('#username').trigger('keydown', {keyCode: 9});
+            cy.get('#username').trigger('keydown', { keyCode: 9 });
             cy.get('#password').type('testingisfun99\n');
-            cy.get('#login-btn').click({force: true});
+            cy.get('#login-btn').click({ force: true });
 
-            cy.get('span.username', {timeout: 10000}).should('have.text', username);
+            cy.get('span.username', { timeout: 10000 }).should('have.text', username);
         }, {
             validate() {
                 cy.get(".username").should('have.text', username);
-            },
+            }
         });
     };
 
-    it('add product to cart', () => {
-        login('demouser')
+    // beforeEach(() => {
+    //     login('demouser');
+    // });
+
+    it('adds product to cart', () => {
+        login('demouser');
         cy.visit("https://bstackdemo.com");
         cy.get("div[data-sku='iPhone12-device-info.png'] div")
             .contains("Add to cart")
@@ -27,4 +31,10 @@ describe('Session usage example', () => {
         cy.get('p.title').should('have.text', 'iPhone 12');
     });
 
+    it('views orders and confirms empty state', () => {
+        login('demouser');
+        cy.visit("https://bstackdemo.com");
+        cy.get("#orders").click();
+        cy.get(".fit h2").should('have.text', 'No orders found');
+    });
 });
