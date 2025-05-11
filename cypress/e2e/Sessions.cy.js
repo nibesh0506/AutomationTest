1fbf04528759226d48928d5360c51d0589b12723
Nibesh Upadhyay <Nibesh.Upadhyay@swifttech.com>
2025-05-11 10:19:22 +0545

    sessions test updated

--- cypress/e2e/Sessions.cy.js
+++ cypress/e2e/Sessions.cy.js
@@ -4,11 +4,11 @@ describe('Session usage example', () => {
             cy.visit("https://bstackdemo.com/signin");
 
             cy.get('#username').type(username);
-            cy.get('#username').trigger('keydown', { keyCode: 9 });
+            cy.get('#username').trigger('keydown', {keyCode: 9});
             cy.get('#password').type('testingisfun99\n');
-            cy.get('#login-btn').click({ force: true });
+            cy.get('#login-btn').click({force: true});
 
-            cy.get('span.username', { timeout: 10000 }).should('have.text', username);
+            cy.get('span.username', {timeout: 10000}).should('have.text', username);
         }, {
             validate() {
                 cy.get(".username").should('have.text', username);
@@ -16,25 +16,29 @@ describe('Session usage example', () => {
         });
     };
 
-    // beforeEach(() => {
-    //     login('demouser');
-    // });
-
     it('adds product to cart', () => {
-        login('demouser');
+        login('demouser')
         cy.visit("https://bstackdemo.com");
         cy.get("div[data-sku='iPhone12-device-info.png'] div")
-            .contains("Add to cart")
-            .first()
+            .filter(":contains('Add to cart')")
+            .eq(2)
             .click();
 
-        cy.get('p.title').should('have.text', 'iPhone 12');
+        //this allows to choose the select after the filter is used.
+
+        cy.get('p.title').should('have.text', 'iPhone 12 Pro Max');
     });
 
     it('views orders and confirms empty state', () => {
-        login('demouser');
+        login('demouser')
         cy.visit("https://bstackdemo.com");
         cy.get("#orders").click();
         cy.get(".fit h2").should('have.text', 'No orders found');
     });
+    it('make sure existing user can view orders', () => {
+        login('existing_orders_user')
+        cy.visit('https://bstackdemo.com')
+        cy.get('#orders').click()
+        cy.get('.a-fixed-left-grid-inner').should('have.length.at.least', 10)
+    })
 });
